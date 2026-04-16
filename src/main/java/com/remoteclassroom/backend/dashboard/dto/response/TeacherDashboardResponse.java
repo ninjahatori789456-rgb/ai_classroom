a9c47998c18a6afe55d69644c78ab5abeb06e951
shubhSharma2003 <shubhsharma20032aug@gmail.com>
2026-04-16 19:00:37 +0530

    error fix

--- src/main/java/com/remoteclassroom/backend/dashboard/dto/response/TeacherDashboardResponse.java
+++ src/main/java/com/remoteclassroom/backend/dashboard/dto/response/TeacherDashboardResponse.java
@@ -2,9 +2,13 @@ package com.remoteclassroom.backend.dashboard.dto.response;
 
 import java.util.List;
 
+import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class TeacherDashboardResponse {
 
     private long totalClassesCreated;
